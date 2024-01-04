00586b7e6bff90169b0717f4af0e8efd1970be8f
JYOSNA PHILIP <142787944+jyosnaphilip@users.noreply.github.com>
2024-01-04 10:25:37 +0530

    added two models for restaurant menu

--- hungryStudentProject/restaurants/models.py
+++ hungryStudentProject/restaurants/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+# model for restaurant menu
+
+class Food(models.Model):
+    Food_id=models.UUIDField(auto_created=True,primary_key=True,default=uuid.uuid)
+    Food_Name=models.TextField(max_length=25,blank=)
+    Category=models.TextChoices()
+
+    Image=models.ImageField()
+
+class Restaurant_Food_bridge(models.Model):
